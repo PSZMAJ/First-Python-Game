3960468c2e23a1e6f8676d1e1d5293a2b96c954a
Przemyslaw Szmaj <przemoszmaj@gmail.com>
2020-08-12 22:22:34 +0200

    ver4 final

--- GAME.py
+++ GAME.py
@@ -30,23 +30,27 @@ JEŚLI PRZEGRASZ, SYSTEM AUTOMATYCZNIE POPROSI CIĘ O SUBSKRYBCJĘ KANAŁU AUTOR
     time.sleep(4)
     print('Ładuję grę...')
     time.sleep(2)
-    name = str(input('Graczu podaj swoje imie: '))
+    name = input('Graczu podaj swoje imie: ')
     # sprawdzenie imienia
-    while name.isalpha() != True:
+    while str(name).isalpha() != True:
          name = input('Graczu, podałeś niepoprawne imie, podaj swoje imie: ')
-   
+    
     print('Twoje imie to {}'.format(name))
     time.sleep(2)
     print('Trwa generowanie liczb...')
     time.sleep(2)
-    tajna_liczba = (random.randint(1,31))
+    tajna_liczba = (random.randint(1,3))
  
     # logika gry
     
     
     for runda in range(2):
-        liczba = int(input('Podaj liczbę z zakresu 1,3'))
-        if liczba == tajna_liczba:
+        liczba = input('Podaj liczbę z zakresu 1,3')
+        if liczba.isdigit():
+            print('')
+        else:
+            print('nie podales liczby')
+        if int(liczba) == tajna_liczba:
             print('Brawo, trafiłeś')
             break
         else:
@@ -65,7 +69,53 @@ JEŚLI PRZEGRASZ, SYSTEM AUTOMATYCZNIE POPROSI CIĘ O SUBSKRYBCJĘ KANAŁU AUTOR
 
     
 def ENG_GAME():
-    print('test')
+    time.sleep(2)
+    print("""
+          
+GAME DESCRIPTION:
+GUESSING NUMBER GAME
+If You win, You can work in your computer.
+If You lose my game will open a browser and i get your subscribe on my YouTube channel.
+          """)  
+          
+    time.sleep(4)
+    print('Loading...')
+    time.sleep(2)
+    name = input('Your name: ')
+    # name chceck 
+    while str(name).isalpha() != True:
+         name = input('Sorry! Your name is incorrect! Please try again : ')
+    
+    print('Your name is:  {}'.format(name))
+    time.sleep(2)
+    print('Generating numbers...')
+    time.sleep(2)
+    secretNumber = (random.randint(1,3))
+ 
+    # logika gry
+    
+    
+    for runda in range(2):
+        number = input('Please enter a numer in the range 1,3')
+        if number.isdigit():
+            print('')
+        else:
+            print('is not a digit')
+        if int(number) == secretNumber:
+            print('You Win')
+            break
+        else:
+            runda = runda + 1
+            print('Your number is incorrect.')
+            if runda == 2:
+                print('You lose... , the number is  {}.'.format(secretNumber))
+                time.sleep(3)
+                # ===> uruchomienie okna przeglądarki i szukanie tesktu 'SUBSKRYBUJ' a następnie kliknięcie.
+                browser = webdriver.Firefox()
+                browser.get('https://www.youtube.com/channel/UC0WP5P-ufpRfjbNrmOWwLBQ')
+                linkElm = browser.find_element_by_link_text('SUBSKRYBUJ')
+                type(linkElm)
+                linkElm.click()
     
 language = int(input('PLEASE SELECT LANGUAGE: '))
 
