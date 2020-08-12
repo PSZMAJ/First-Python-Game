fd070c973d6c2e8e7f7abe2223e6a2ef86640f2a
Przemyslaw Szmaj <przemoszmaj@gmail.com>
2020-08-12 18:31:24 +0200

    second commit

--- GAME.py
+++ GAME.py
@@ -20,40 +20,57 @@ print('2-ENG')
 def PL_GAME():
     time.sleep(2)
     print("""
-          ZASADY GRY:
+OPIS GRY:
 GRA POLEGA NA ZGADNIĘCIU LICZBY Z PEWNEGO ZAKRESU.
 JEŚLI ZGADNIESZ LICZBĘ, TO WYGRYWASZ I MOŻESZ PRACOWAĆ NA SWOIM KOMPUTERZE DALEJ.
 JEŚLI PRZEGRASZ, SYSTEM AUTOMATYCZNIE POPROSI CIĘ O SUBSKRYBCJĘ KANAŁU AUTORA NA YOUTUBE.
           """)
+          
+          
+          
+          
     time.sleep(4)
     print('Ładuję grę...')
     time.sleep(2)
-    name = input('Graczu podaj swoje imie: ')
+    name = str(input('Graczu podaj swoje imie: '))
     
-    while name.isdigit() != False:
-        name = input('podaj imie: ')
+    while name.isalpha() != True:
+         name = input('Graczu, podałeś niepoprawne imie, podaj swoje imie: ')
+    
+    #while name.isdigit() != False:
+     #    name = str(input('Graczu, podałeś niepoprawne imie, podaj swoje imie: '))
         
     print('Twoje imie to {}'.format(name))
     time.sleep(2)
     print('Trwa generowanie liczb...')
     time.sleep(2)
-    tajna_liczba = (random.randint(1,3))
+    tajna_liczba = (random.randint(1,31))
     #print(tajna_liczba)
     
-    for i in range(2):
+    
+    
+    
+    
+    
+    for runda in range(2):
         liczba = int(input('Podaj liczbę z zakresu 1,3'))
     
         if liczba == tajna_liczba:
             print('Brawo, trafiłeś')
             break
         else:
-            print('Spróbuj jeszcze raz...')
-            time.sleep(2)
-    print('Przegrałeś, wylosowana liczba to {}.'.format(tajna_liczba))
-    print('koniec gry, teraz czekam na suba')
-    time.sleep(2)
-    webbrowser.open('https://www.youtube.com/channel/UCewT7Lr5f6LWvqSPXm0JKRw?sub_confirmation=1')
-    #driver.find_element_by_css_selector(class="style-scope yt-button-renderer style-blue-text size-default").click() 
+            runda = runda + 1
+            print('Podałeś błędną liczbę.')
+            if runda == 2:
+                print('Przegrałeś, wylosowana liczba to {}.'.format(tajna_liczba))
+                time.sleep(3)
+                webbrowser.open('https://www.youtube.com/channel/UCewT7Lr5f6LWvqSPXm0JKRw?sub_confirmation=1')
+        
+    
+    
+    
+    
+    
     
 def ENG_GAME():
     print('test')
