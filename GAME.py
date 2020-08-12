1228c5ad90b94c3ec5d4d231fb96861604d41ad3
Przemyslaw Szmaj <przemoszmaj@gmail.com>
2020-08-12 21:39:48 +0200

    ver3

--- GAME.py
+++ GAME.py
@@ -8,8 +8,8 @@ Created on Sun Aug  9 01:23:22 2020
 
 import time
 import random
-import webbrowser
 from selenium import webdriver 
+
 
 print('LOADING GAME')
 time.sleep(2)
@@ -20,41 +20,32 @@ print('2-ENG')
 def PL_GAME():
     time.sleep(2)
     print("""
+          
 OPIS GRY:
 GRA POLEGA NA ZGADNIĘCIU LICZBY Z PEWNEGO ZAKRESU.
 JEŚLI ZGADNIESZ LICZBĘ, TO WYGRYWASZ I MOŻESZ PRACOWAĆ NA SWOIM KOMPUTERZE DALEJ.
 JEŚLI PRZEGRASZ, SYSTEM AUTOMATYCZNIE POPROSI CIĘ O SUBSKRYBCJĘ KANAŁU AUTORA NA YOUTUBE.
-          """)
-          
-          
-          
+          """)  
           
     time.sleep(4)
     print('Ładuję grę...')
     time.sleep(2)
     name = str(input('Graczu podaj swoje imie: '))
-    
+    # sprawdzenie imienia
     while name.isalpha() != True:
          name = input('Graczu, podałeś niepoprawne imie, podaj swoje imie: ')
-    
-    #while name.isdigit() != False:
-     #    name = str(input('Graczu, podałeś niepoprawne imie, podaj swoje imie: '))
-        
+   
     print('Twoje imie to {}'.format(name))
     time.sleep(2)
     print('Trwa generowanie liczb...')
     time.sleep(2)
     tajna_liczba = (random.randint(1,31))
-    #print(tajna_liczba)
-    
-    
-    
-    
+ 
+    # logika gry
     
     
     for runda in range(2):
         liczba = int(input('Podaj liczbę z zakresu 1,3'))
-    
         if liczba == tajna_liczba:
             print('Brawo, trafiłeś')
             break
@@ -64,20 +55,17 @@ JEŚLI PRZEGRASZ, SYSTEM AUTOMATYCZNIE POPROSI CIĘ O SUBSKRYBCJĘ KANAŁU AUTOR
             if runda == 2:
                 print('Przegrałeś, wylosowana liczba to {}.'.format(tajna_liczba))
                 time.sleep(3)
-                webbrowser.open('https://www.youtube.com/channel/UCewT7Lr5f6LWvqSPXm0JKRw?sub_confirmation=1')
-        
+                # ===> uruchomienie okna przeglądarki i szukanie tesktu 'SUBSKRYBUJ' a następnie kliknięcie.
+                browser = webdriver.Firefox()
+                browser.get('https://www.youtube.com/channel/UC0WP5P-ufpRfjbNrmOWwLBQ')
+                linkElm = browser.find_element_by_link_text('SUBSKRYBUJ')
+                type(linkElm)
+                linkElm.click()
     
-    
-    
-    
-    
+
     
 def ENG_GAME():
     print('test')
-    driver = webdriver.Chrome()
-    driver.get("https://www.youtube.com/channel/UCewT7Lr5f6LWvqSPXm0JKRw/featured")
-    button = driver.find_element_by_id('text')
-    button.click()
     
 language = int(input('PLEASE SELECT LANGUAGE: '))
 
